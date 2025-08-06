631f08de6774365ad54b17350beba41e38cbc6e1
alexsteixeira00 <alexs.juci@gmail.com>
2025-08-06 03:13:28 -0300

    meu portfolio

---
+++ scripts.js
@@ -0,0 +1,13 @@
+
+
+tsParticles.load("tsparticles", {
+  fullScreen: { enable: true },
+  background: { color: "#121212" },
+  particles: {
+    color: { value: "#ffffff" },
+    links: { enable: true, color: "#ffffff", distance: 100 },
+    move: { enable: true, speed: 1 },
+    number: { value: 50 },
+    size: { value: 2 },
+  },
+});
